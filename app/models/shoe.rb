84106cae6caf7da9f960159a9079cd57c759e41b
Jonathan Monsalve <j.jmonsalveg@gmail.com>
2017-08-06 22:36:13 -0400

    Style/Chore: Add alerts and custom theme,gem better errors and livereload

--- app/models/shoe.rb
+++ app/models/shoe.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: shoes
+#
+#  id           :integer          not null, primary key
+#  model        :string
+#  brand        :string
+#  isbn         :string
+#  sku          :string
+#  release_year :integer
+#  edition_date :date
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class Shoe < ApplicationRecord
+  validates :brand , presence: true
 end
